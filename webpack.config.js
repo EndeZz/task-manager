ddfb212d8fffcdffda25f829c32c42f7f847ddd3
nutatio <dar489@gmail.com>
2021-08-27 20:06:43 +0400

    htmlwebpack, eslint config, stylelint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');  
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
+  mode:"development",
   entry:"./src/index.js",
   output:{
-    filename:"main.js",
+    filename:"main.[contenthash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    }),
+    // new ESLintPlugin({
+    //   context: "./src/",
+    //   extensions: [".js"],
+    // }),
+    // new StylelintPlugin(),
+  ],
+  
+
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          "style-loader", //2. inject styles into dom
+          "css-loader"    //1. css -> commonjs
+          ]
       }
     ]
   }
