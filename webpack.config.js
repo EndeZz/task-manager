46c9370b381c43dd7aea25979f5879d7c3b802a7
elizaveta <elizavetaBun@gmail.com>
2021-08-31 22:46:25 +0400

    added style-loader, css-loader, sass-loader, file-loader,plugins: htmlwebpack, eslint, stylelint. and .vscode settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,9 +9,31 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif|ico)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'images',
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html'),
     }),
+    new ESLintPlugin({
+      context: path.join(__dirname, 'src'),
+      extensions: ['.js'],
+    }),
+    new StylelintPlugin(),
   ],
 };
