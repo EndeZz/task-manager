b48090e68f926271e8612a4370b681ea76cc9057
Urakova <gram_anna@mail.ru>
2021-09-01 17:00:00 +0400

    init: Создан проект. Сформирована начальная структура файлов

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const ESLintPlugin = require('eslint-webpack-plugin');
+// const StylelintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      }
+    ]
+  },
+  devServer: {
+    port: 5000
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     { from: path.resolve(__dirname, './src/file'), to: path.resolve(__dirname, './dist/file') }
+    //   ],
+    // }),
+    // new ESLintPlugin({
+    //   extensions: ['ts', 'tsx', 'js', 'jsx']
+    // }),
+    // new StylelintPlugin({
+    //   extensions: ['css', 'scss', 'sass']
+    // })
+  ]
+}
