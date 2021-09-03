e3c72e7c06afa79f594ab9ffec6809a93f10a721
Ксения Ефимычева <Efymichevaka@vsudur.local>
2021-09-03 11:30:45 +0400

    init: load project

---
+++ kefimycheva-project/kefimycheva-week3-project/webpack/dev.js
@@ -0,0 +1,17 @@
+const { merge } = require('webpack-merge');
+const paths = require('./path');
+
+const common = require('./common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    compress: true,
+    contentBase: paths.build,
+    historyApiFallback: true,
+    open: true,
+    port: 3000,
+    hot: true,
+  },
+});
