eb6782f0aa799f9df6bca01529b1ddbcec3d7fec
ibusyrev <spaik-98@mail.ru>
2021-09-04 18:53:20 +0400

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
 const ESLintPlugin = require('eslint-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path")
 
 module.exports = {
   plugins: [
     new ESLintPlugin(),
-    new StylelintPlugin()],
+    new StylelintPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      linkType: "text/css",
+      filename: "style.css",
+      chunkFilename: "[id].css"
+    }),],
+
+
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
 };
