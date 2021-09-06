46c2ac9989335a9f14ba4dac5379dd41eb292faf
Ксения Ефимычева <Efymichevaka@vsudur.local>
2021-09-06 11:33:27 +0400

    add: styles for cards

--- kefimycheva-project/kefimycheva-week3-project/src/js/main.js
+++ kefimycheva-project/kefimycheva-week3-project/src/js/main.js
@@ -19,6 +19,15 @@ const inputCalendar = document.querySelector('.input__block-text--calendar');
 const calendarCreate = new Calendar();
 calendar.append(calendarCreate.render());
 
+const dayNow = (new Date()).getDate();
+const calendarDays = document.querySelectorAll('.calendar__days-item__num');
+
+calendarDays.forEach((day) => {
+  if (+day.textContent === dayNow) {
+    day.classList.add('calendar__days-item__num--today');
+  }
+});
+
 inputCalendar.addEventListener('click', () => {
   document.querySelector('.calendar__wrapper').classList.toggle('show');
 });
