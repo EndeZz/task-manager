61073d48ec5746b2736ea8fedce9776aaf25cd7b
Vladimir Tonkov <vovatonkov@yandex.ru>
2021-09-11 00:17:48 +0400

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
+import '../css/simplebar.css';
+import '../css/jsCalendar.css';
 import '../scss/style.scss';
-import '../index.html'
+import '../index.html';
+
+// JS
+import './modules/simplebar';
+import './modules/jsCalendar';
+import './modules/jsCalendar.lang.ru';
+
+let profile = document.querySelector('.header__user-photo');
+
+profile.addEventListener('click', () => {
+  document.querySelector('.header__user-profile').classList.toggle('photo-active');
+});
