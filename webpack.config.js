4769de56ef1cbca0a18ab6523b9dba15909df0a8
elizaveta <elizavetaBun@gmail.com>
2021-09-27 21:34:25 +0400

    fix: change js to jsx? type of file in babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,20 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.?js$/,
+        test: /\.?jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [
+              '@babel/preset-env',
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic',
+                },
+              ],
+            ],
           },
         },
       },
