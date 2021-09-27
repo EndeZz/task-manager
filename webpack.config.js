f81be00b51f2e4705bd7011e89ab58bd2678e374
elizaveta <elizavetaBun@gmail.com>
2021-09-27 21:30:32 +0400

    refactor: add config to import and bundle images

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,15 @@ module.exports = {
         test: /\.scss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jp(e*)g|svg|gif|ico)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'images',
+          },
+        },
+      },
     ],
   },
   plugins: [
