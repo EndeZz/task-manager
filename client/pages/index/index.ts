bf3c6bab059743541db59a6c80d82628fec1b4a3
endezz <r-serega13@yandex.ru>
2021-10-06 12:50:44 +0400

    fix: fixing missed eslint errors in ts files

--- client/pages/index/index.ts
+++ client/pages/index/index.ts
@@ -4,11 +4,9 @@ import '../../components/control/control';
 import '../../components/feed/feed';
 import '../../components/lightbox/lightbox';
 import '../../components/loader/loader';
-import '../../components/calendar/calendar';
 
 import './index.scss';
 
-import '../../js/calendar';
 import '../../js/dropdown';
 import '../../js/lightbox';
 import '../../js/filter';
