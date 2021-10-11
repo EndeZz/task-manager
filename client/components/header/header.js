e090440838c16416fd9003c061c3783e513ab3ba
Urakova <gram_anna@mail.ru>
2021-10-11 05:39:37 +0400

    feat: определение текущей страницы вынесено в JS

--- client/components/header/header.js
+++ client/components/header/header.js
@@ -4,9 +4,26 @@ const buttonMenu = document.querySelector('.user__button');
 const menuUser = document.querySelector('.user-menu');
 
 buttonMenu.addEventListener('click', () => {
-  buttonMenu.classList.add('user__button_active')
+  buttonMenu.classList.toggle('user__button_active')
 })
 
 menuUser.addEventListener('mouseleave', () => {
   buttonMenu.classList.remove('user__button_active')
 })
+
+const location = window.location.pathname.match(/(?<=\/)\w+(?=\.html)/ig)[0];
+const page = {
+  'index': [document.getElementsByClassName('menu__item')[0], 'menu__item'],
+  'task': [document.getElementsByClassName('menu__item')[1], 'menu__item'],
+  'tasks': [document.getElementsByClassName('menu__item')[1], 'menu__item'],
+  'new-task': [document.getElementsByClassName('menu__item')[1], 'menu__item'],
+  'user': [document.getElementsByClassName('menu__item')[2], 'menu__item'],
+  'users': [document.getElementsByClassName('menu__item')[2], 'menu__item'],
+  'profile': [document.getElementsByClassName('menu__item')[0], 'menu__item'],
+  'notifications': [document.querySelector('.notification'), 'notification']
+};
+
+if (page.hasOwnProperty(location)) {
+  page[location][0].classList.add(page[location][1] + '_active');
+}
+
