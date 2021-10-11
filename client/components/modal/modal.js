9d9738702befe16afe0734d3eef51802bc0e1d50
Urakova <gram_anna@mail.ru>
2021-10-11 06:07:07 +0400

    feat: сверстаны просмотр данных в модал. окне

---
+++ client/components/modal/modal.js
@@ -0,0 +1,62 @@
+import './modal.scss';
+import durationTime from '../../utils/time'
+
+const types = {
+  video: 'Видео',
+  photo: 'Фото',
+  audio: 'Аудио'
+};
+
+
+export default function (data) {
+  const modal = document.querySelector('.modal');
+
+  modal.onclick = function () {
+    modal.remove()
+  }
+
+  document.getElementsByClassName('media-button-back')[0].addEventListener(('click'), function () {
+    document.querySelector('.modal').style.display = "none";
+    modal.remove()
+  })
+
+
+  modal.style.display = "block";
+
+  modal.querySelector('.modal__content').classList.add('modal__content_' + data[0].type.name)
+
+  modal.querySelector('.type__img').classList.add('type__img_' + data[0].type.name);
+  modal.querySelector('.type__img > use').href.baseVal += data[0].type.name;
+  modal.querySelector('.type__text').innerText = types[data[0].type.name];
+
+  modal.querySelector('.modal__title').innerText = data[0].name;
+  modal.querySelector('.modal__date').innerText = data[0].dataCreated;
+
+  const component = modal.getElementsByClassName('modal__component');
+  let time = "";
+
+  switch (data[0].type.name) {
+    case 'photo': {
+      component[1].setAttribute('src', data[0].url);
+      break;
+    }
+
+    case 'video': {
+      component[0].setAttribute('src', data[0].url);
+      component[0].onloadedmetadata = function () {
+        time = durationTime(Math.floor(component[0].duration));
+        modal.querySelector('.modal__type-time').innerText = time;
+      }
+      break;
+    }
+
+    case 'audio': {
+      component[2].setAttribute('src', data[0].url);
+      component[2].onloadedmetadata = function () {
+        time = durationTime(Math.floor(component[2].duration));
+        modal.querySelector('.modal__type-time').innerText = time;
+      }
+      break;
+    }
+  }
+}
