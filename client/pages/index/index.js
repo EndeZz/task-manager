11266790e63b7f029207ed16cafdf1454a99ae1c
Urakova <gram_anna@mail.ru>
2021-10-11 05:57:02 +0400

    feat: создан компонент type, подключены чекбоксы

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,3 +1,6 @@
 import './index.scss';
+
 import '../../components/header/header';
+import '../../components/type/type';
 import '../../components/search/search';
+import '../../components/checkbox/checkbox';
