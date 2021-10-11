82d4343713821f3aafd870e10a48adb91ee8ec50
Urakova <gram_anna@mail.ru>
2021-10-11 06:13:22 +0400

    feat: кастомный селект для выбора статуса задачи

---
+++ client/components/select/select.js
@@ -0,0 +1,25 @@
+import './select.scss';
+
+const selectBox = document.getElementsByClassName('select-box');
+
+
+Array.from(selectBox).forEach((select) => {
+  const input = select.querySelector('.select')
+  input.addEventListener('click', () => {
+    select.classList.toggle('select-box_active')
+  })
+
+  const items = select.getElementsByClassName('select__item');
+
+
+  Array.from(items).forEach((item) => {
+    item.addEventListener('click', () => {
+      input.style.color = getComputedStyle(item).color;
+      input.style.fontWeight = +getComputedStyle(item).fontWeight;
+      input.value = item.innerText;
+      select.classList.toggle('select-box_active');
+    })
+  })
+})
+
+
--- client/pages/tasks/tasks.js
+++ client/pages/tasks/tasks.js
@@ -1,4 +1,7 @@
-import '../../components/header/header';
-
 import './tasks.scss';
 
+import '../../components/header/header';
+import '../../components/search/search';
+import '../../components/checkbox/checkbox';
+import '../../components/select/select';
+import '../../components/type/type';
