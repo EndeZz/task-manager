01656317cf45b76fd7d45f7309f128a2ea213343
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-08 00:19:30 +0400

    feat: add file component

---
+++ src/components/file/file.tsx
@@ -0,0 +1,17 @@
+import ButtonMain from "../buttons/buttonMain/buttonMain";
+import './file.scss';
+
+function File(props: { type: string, name: string, size: string } ) {
+  return (
+    <div className="file">
+      <div className="file__info">
+        <img src={`/public/icons/${props.type}.svg`} alt="" />
+        <span className="file__info__name">{props.name}</span>
+        <span className="file__info__size">{props.size}</span>
+      </div>
+      <ButtonMain type='download' color='lightblue' text='Скачать'></ButtonMain>
+    </div>
+  );
+}
+
+export default File;
