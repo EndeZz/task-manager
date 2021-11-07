9a826f5fe165900d6c4dd2a8ed3815d862b94307
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-08 00:15:50 +0400

    feat: add card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,20 @@
+import ContentLabel from '../labels/ContentLabel/ContentLabel';
+import './card.scss';
+
+export default function CardComponent(props: {type: string, duration: string, name: string, author: string, date: string}) {
+  return (
+      <div className='card'>
+        <img src="../../../public/img/beach.jfif" alt="Пляж" />
+        <div className="card__type">
+          <ContentLabel type='video'></ContentLabel>
+          <span className={`duration__${props.type}`}>{props.duration}</span>
+        </div>
+        <span>{props.name}</span>
+        <div className="card__text ">
+          <span className="card__date">{props.author}</span>
+          <img src="../../../public/icons/separator.svg" alt="Разделитель" />
+          <time className="time.card__date">{props.date}</time>
+        </div>
+      </div>
+  );
+}
