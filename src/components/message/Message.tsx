a9663707950f315a29998ed84b95f9931e636465
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-08 00:21:04 +0400

    feat: add message component

---
+++ src/components/message/Message.tsx
@@ -0,0 +1,93 @@
+import './message.scss';
+
+function Message(props: { type: string, author: string, task: string, date: string }) {
+  return (
+    <div className="message">
+      {(() => {
+        switch (props.type) {
+          case 'comment':
+            return (
+              <div>
+                <img className="message__icon" src="/public/icons/comment.svg" alt="Новый комментарий" />
+                <div className="message__text">
+                  <p>Новый комментарий в задаче 
+                    <span className="message__text-bold">{props.task}</span>
+                  </p>
+                  <span className="message__date">{props.date}</span>
+                </div>
+              </div>
+            )
+          case 'attach':
+            return (
+              <div>
+                <img className="message__icon" src="/public/icons/attach.svg" alt="Новый контент" />
+                <div className="message__text">
+                  <p>Новый комментарий в задаче 
+                    <span className="message__text-bold">{props.task}</span>
+                    <span> загрузил контент в задаче </span>
+                    <span className="message__text-bold">{props.task}</span>
+                  </p>
+                  <span className="message__date">{props.date}</span>
+                </div>
+              </div>
+            )
+            case 'attach':
+              return (
+                <div>
+                  <img className="message__icon" src="/public/icons/attach.svg" alt="Новый контент" />
+                  <div className="message__text">
+                    <p>Новый комментарий в задаче 
+                      <span className="message__text-bold">{props.author}</span>
+                      <span> загрузил контент в задаче </span>
+                      <span className="message__text-bold">{props.task}</span>
+                    </p>
+                    <span className="message__date">{props.date}</span>
+                  </div>
+                </div>
+              )
+          case 'task_video':
+            return (
+              <div>
+                <img className="message__icon" src="/public/icons/video-notification.svg" alt="Новая задача" />
+                <div className="message__text">
+                  <p>
+                    <span className="message__text-bold">{props.author}</span>
+                    <span> поручил вам новую задачу</span>
+                  </p>
+                  <span className="message__date">{props.date}</span>
+                </div>
+              </div>
+            )
+          case 'task_photo':
+            return (
+              <div>
+                <img className="message__icon" src="/public/icons/photo-notification.svg" alt="Новая задача" />
+                <div className="message__text">
+                  <p>
+                    <span className="message__text-bold">{props.author}</span>
+                    <span> поручил вам новую задачу</span>
+                  </p>
+                  <span className="message__date">{props.date}</span>
+                </div>
+              </div>
+            )
+          case 'audio_video':
+            return (
+              <div>
+                <img className="message__icon" src="/public/icons/audio-notification.svg" alt="Новая задача" />
+                <div className="message__text">
+                  <p>
+                    <span className="message__text-bold">{props.author}</span>
+                    <span> поручил вам новую задачу</span>
+                  </p>
+                  <span className="message__date">{props.date}</span>
+                </div>
+              </div>
+            )
+          }  
+      })()}
+    </div>
+  );
+}
+
+export default Message;
