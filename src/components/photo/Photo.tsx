1402388742630dcc3158a9c03cec9a498b3b423d
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-08 00:22:16 +0400

    feat: add photo component

---
+++ src/components/photo/Photo.tsx
@@ -0,0 +1,17 @@
+import ButtonBig from '../buttons/ButtonBig/ButtonBig';
+import './photo.scss';
+
+function Photo(props: { text: string }) {
+  return (
+    <div className="photo">
+      <div className="photo__img">
+        <img src="/public/icons/profile.svg" alt="Изображение профиля" />
+      </div>
+      <div className="photo__button">
+        <ButtonBig type='upload' color='blue' text='Загрузить аватар'></ButtonBig>
+      </div>
+    </div>
+  );
+}
+
+export default Photo;
