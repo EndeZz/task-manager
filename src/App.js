9d7f4017ade1ce7bfe78a21c3b615a3d25ca34e6
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-07 15:44:15 +0400

    repo init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AuthComponent from './pages/auth/auth';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthComponent></AuthComponent>
+    </div>
+  );
+}
+
+export default App;
