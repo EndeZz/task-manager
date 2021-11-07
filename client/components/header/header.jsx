a72e1518131f569f8b76fa22a8b9fcd34d17f8c9
Vladimir Tonkov <vovatonkov@yandex.ru>
2021-11-08 03:09:30 +0400

    init: react app

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Navigation from '../navigation/navigation';
 
+import Index from '../../pages/index';
+import Tasks from '../../pages/tasks/tasks';
+import Users from '../../pages/users/users';
 
-
-const Header = (props) => {
+const Header = (  props  ) => {
 
     console.log("setIs", props)
 
@@ -16,32 +18,33 @@ const Header = (props) => {
     
         return pug`
             ${props.children[1].map((item, ind) => pug`
-                div(class=${item.className}) 
-                    .container 
-                        .header-block  
-                            .header__logo-img 
-                                a 
-                                    img(src=${item.logoImg})
-                            nav.header__menu        
-                                ul.header__menu-list
-                                    li.header__menu-item
-                                        a(href="/") Главная
-                                    li.header__menu-item
-                                        a(href="/tasks") Задачи
-                                    li.header__menu-item
-                                        a(href="/users") Пользователи
-                            .header__notif-block                
-                                a(class="header__notif-link header__notif-link_border", href="#")
-                                    img(src=${item.notifImg})
-                                    span(class=${item.classNotif}) ${item.notifCount}
-                            .header__user-block ${item.nameUser}
-                            .header__user-photo 
-                                img.header__user-img(src=${item.logoPers} alt="Логотип пользователя")
-                            ul.header__user-profile 
-                                li.header__user-item 
-                                    a.header__user-log(href="#") Профиль
-                                li.header__user-item 
-                                    a.header__user-goout(href="#") Выход
+                Router
+                    div(class=${item.className}) 
+                        .container 
+                            .header-block  
+                                .header__logo-img 
+                                    a 
+                                        img(src=${item.logoImg})
+                                nav.header__menu        
+                                    ul.header__menu-list
+                                        li.header__menu-item
+                                            a(href="/") Главная
+                                        li.header__menu-item
+                                            a(href="/tasks") Задачи
+                                        li.header__menu-item
+                                            a(href="/users") Пользователи
+                                .header__notif-block                
+                                    a(class="header__notif-link header__notif-link_border", href="#")
+                                        img(src=${item.notifImg})
+                                        span(class=${item.classNotif}) ${item.notifCount}
+                                .header__user-block ${item.nameUser}
+                                .header__user-photo 
+                                    img.header__user-img(src=${item.logoPers} alt="Логотип пользователя")
+                                ul.header__user-profile 
+                                    li.header__user-item 
+                                        a.header__user-log(href="#") Профиль
+                                    li.header__user-item 
+                                        a(class="header__user-goout" href="#" onClick=${handleLogOut}) Выход
             `)} 
         `;
 }
