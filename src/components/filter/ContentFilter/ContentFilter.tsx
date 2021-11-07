44eea4231982038bc45db53ebec18580b7cb705f
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-08 00:18:24 +0400

    feat: add filters components

---
+++ src/components/filter/ContentFilter/ContentFilter.tsx
@@ -0,0 +1,18 @@
+import ButtonContent from '../../buttons/ButtonContent/ButtonContent';
+import InputLabelPattern from '../../input/InputLabelPattern/InputLabelPattern';
+import './ContentFilter.scss';
+
+function ContentFilter() {
+  return (
+    <div className="filter_content">
+      <InputLabelPattern label='Тип контента' id=''></InputLabelPattern>
+      <div className="filter__buttons">
+        <ButtonContent type='video' text='Видео'></ButtonContent>
+        <ButtonContent type='photo' text='Фото'></ButtonContent>
+        <ButtonContent type='audio' text='Аудио'></ButtonContent>
+      </div>
+    </div>
+  );
+}
+
+export default ContentFilter;
---
+++ src/components/filter/RoleFilter/RoleFilter.tsx
@@ -0,0 +1,19 @@
+import ButtonFilter from '../../buttons/ButtonFilter/ButtonFilter';
+import InputLabelPattern from '../../input/InputLabelPattern/InputLabelPattern';
+import './RoleFilter.scss';
+
+function RoleFilter() {
+  return (
+    <div className="filter_role">
+      <InputLabelPattern label='Роль' id=''></InputLabelPattern>
+      <div className="filter_role__buttons">
+        <ButtonFilter text='Все'></ButtonFilter>
+        <ButtonFilter text='Контент-мейкер'></ButtonFilter>
+        <ButtonFilter text='Менеджер'></ButtonFilter>
+        <ButtonFilter text='Администратор'></ButtonFilter>
+      </div>
+    </div>
+  );
+}
+
+export default RoleFilter;
