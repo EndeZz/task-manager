b5ffc4cf42bfbad8af7a89fdd4dd3f524ed04050
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-08 11:51:31 +0400

    feat: add routes

---
+++ src/router/routes.ts
@@ -0,0 +1,16 @@
+const ROUTER = {
+  PAGES: {
+      INDEX: '/',
+      ERROR: '/404',
+      AUTH: '/auth',
+      NOTIFICATIONS: '/notifications',
+      PROFILE: '/profile',
+      TASKNEW: '/task-new',
+      TASK: '/task',
+      TASKS: '/tasks',
+      USER: '/user',
+      USERS: '/users',
+  }
+};
+
+export default ROUTER;
