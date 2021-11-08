ef8977bedbcc85e98bdf4f17a7ab490a32476cec
Urakova <gram_anna@mail.ru>
2021-11-08 11:18:34 +0400

    refactor: Удаление PUG, перевод js на tsx

--- client/pages/404/404.js
+++
@@ -1,5 +0,0 @@
-import './404.scss';
-
-import '../../components/header/header';
-import '../../components/button-with-text/button-with-text';
-import '../../components/user-avatar/user-avatar';
--- client/pages/auth/auth.js
+++
@@ -1,9 +0,0 @@
-import './auth.scss';
-
-import '../../components/button-with-text/button-with-text';
-import '../../components/user-avatar/user-avatar';
-
-document.querySelector('.button-login').onclick = () => {
-  localStorage.id = 1;
-  window.location.href = 'index.html';
-};
