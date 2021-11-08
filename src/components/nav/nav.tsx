58278dd5a81a5a936b07252df545b79add1ebff3
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-08 11:48:10 +0400

    fix: change conditions

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,39 +1,37 @@
 import './nav.scss';
 
-function Menu() {
-  return (
-    <div>
-      <li className="button nav__btn">
-        <a href="/">Главная</a>
-      </li >
-      <li className="button nav__btn">
-        <a href="/tasks">Задачи</a>
-      </li>
-    </div>
-  )
-}
-
-function MenuAdmin() {
-  return (
-    <div>
-      <Menu></Menu>
-      <li className="button nav__btn">
-        <a href="/users">Пользователи</a>
-      </li>
-    </div>
-  )
-}
-
 function Nav(props: { role: "admin" | "manager" | "content-maker" }) {
   return (
     <div className="nav">
-      <ul className="nav__list">
-          {
-            props.role === 'admin' 
-            ? <MenuAdmin></MenuAdmin>
-            : <Menu></Menu>
-          }
-      </ul>
+      {(() => {
+          switch (props.role) {
+            case 'admin':
+              return (
+                <ul className="nav__list">
+                  <li className="button nav__btn">
+                    <a href="/">Главная</a>
+                  </li >
+                  <li className="button nav__btn">
+                    <a href="/tasks">Задачи</a>
+                  </li>
+                </ul>
+              )
+            default:
+              return (
+                <ul className="nav__list">
+                  <li className="button nav__btn">
+                    <a href="/">Главная</a>
+                  </li >
+                  <li className="button nav__btn">
+                    <a href="/tasks">Задачи</a>
+                  </li>
+                  <li className="button nav__btn">
+                    <a href="/users">Пользователи</a>
+                  </li>
+                </ul>
+              )
+            }  
+        })()}
     </div>
   );
 }
