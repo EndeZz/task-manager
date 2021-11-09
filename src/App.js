2a422356f01cfa948642e3903c4715fbf194a2cc
Marina Merzliakova <mrn.mrzlk@gmail.com>
2021-11-09 19:06:58 +0400

    feat: add new pages to the app

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import Notifications from './pages/notifications/notifications';
 import Profile from './pages/profile/profile';
 import Task from './pages/task/Task';
 import TaskNew from './pages/TaskNew/TaskNew';
+import Tasks from './pages/tasks/Tasks';
+import User from './pages/user/User';
+import Users from './pages/users/Users';
 
 const { PAGES } = ROUTER;
 
@@ -28,6 +31,9 @@ function App() {
         <Route path={PAGES.PROFILE} element={<Profile></Profile>}></Route>
         <Route path={PAGES.TASK} element={<Task></Task>}></Route>
         <Route path={PAGES.TASKNEW} element={<TaskNew></TaskNew>}></Route>
+        <Route path={PAGES.TASKS} element={<Tasks></Tasks>}></Route>
+        <Route path={PAGES.USER} element={<User></User>}></Route>
+        <Route path={PAGES.Users} element={<Users></Users>}></Route>
       </Routes>
     </Router>
     </div>
